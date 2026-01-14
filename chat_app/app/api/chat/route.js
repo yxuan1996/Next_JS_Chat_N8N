0e76bee68ec97ff3b1276a6aa419312fe1156a8a
Sim Yi Xuan <yxuan1996@gmail.com>
2026-01-14 11:34:55 +0000

    Change URL for deployment

--- chat_app/app/api/chat/route.js
+++ chat_app/app/api/chat/route.js
@@ -16,7 +16,7 @@ export async function POST(request) {
       currentSessionId = randomUUID()
       
       // Save session to database
-      await fetch(`http://127.0.0.1:3000/api/sessions`, {
+      await fetch(`/api/sessions`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
